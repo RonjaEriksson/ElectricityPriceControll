b101960ada6139f3bf447bdf300f0775db6d5eab
RonjaEriksson <ronja.marie.eriksson@gmail.com>
2023-04-08 12:07:48 +0200

    added price get at startup

--- elpriskoll.py
+++ elpriskoll.py
@@ -2,14 +2,30 @@ import requests
 import json
 from datetime import datetime, timedelta
 last_hour = -1
+now = datetime.now()
 acceptable_price = []
 tomorrows_price = []
+year = now.strftime("%Y")
+
+month = now.strftime("%m")
+
+day = now.strftime("%d")
+
+url = 'https://www.elprisetjustnu.se/api/v1/prices/{current_year}/{current_month}-{current_day}_SE3.json'.format(current_year = year, current_month = month, current_day=day)
+price = requests.get(url)
+json_price = json.loads(price.text)
+#print(json_price)
+for i in range(24):
+    #print(item["SEK_per_kWh"])
+    if json_price[i]["SEK_per_kWh"] < 0.7 :
+        acceptable_price.append(i)
+        print(acceptable_price)
 while True:
     now = datetime.now()+timedelta(1)
     time = int(now.strftime("%H"))
     if last_hour != time:
         last_hour = time
-        if(time == 0 or True) :
+        if(time == 0) :
             acceptable_price = tomorrows_price
         if time in acceptable_price:
             print("on")
