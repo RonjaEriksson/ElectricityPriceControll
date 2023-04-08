c2df705e903594059479b8cf70d8d42de846dd26
RonjaEriksson <ronja.marie.eriksson@gmail.com>
2023-04-08 15:26:19 +0200

    added version which picks x cheapest hours

--- elpriskoll_antal_timmar.py
+++ elpriskoll_antal_timmar.py
@@ -1,10 +1,14 @@
 import requests
 import json
 from datetime import datetime, timedelta
+hours_needed = 5
+def takePrice(elem):
+    return float(elem["SEK_per_kWh"])
+
 last_hour = -1
 now = datetime.now()
-acceptable_price = []
-tomorrows_price = []
+best_hours_today = []
+best_hours_tomorrow = []
 year = now.strftime("%Y")
 
 month = now.strftime("%m")
@@ -15,19 +19,24 @@ url = 'https://www.elprisetjustnu.se/api/v1/prices/{current_year}/{current_month
 price = requests.get(url)
 json_price = json.loads(price.text)
 #print(json_price)
-for i in range(24):
-    #print(item["SEK_per_kWh"])
-    if json_price[i]["SEK_per_kWh"] < 0.7 :
-        acceptable_price.append(i)
-        print(acceptable_price)
+json_price.sort(key=takePrice)
+for i in range(hours_needed):
+    print(json_price[i]["SEK_per_kWh"])
+    time = json_price[i]["time_start"]
+    hours = time.split("T")[1]
+    print(hours)
+    time = int(hours.split(":")[0])
+    print(time)
+    best_hours_today.append(time)
+print(best_hours_today)
 while True:
     now = datetime.now()+timedelta(1)
     time = int(now.strftime("%H"))
     if last_hour != time:
         last_hour = time
         if(time == 0) :
-            acceptable_price = tomorrows_price
-        if time in acceptable_price:
+            best_hours_today = best_hours_tomorrow
+        if time in best_hours_today:
             print("on")
         else:
             print("off")
@@ -45,9 +54,11 @@ while True:
             price = requests.get(url)
             json_price = json.loads(price.text)
 #print(json_price)
-            tomorrows_price = []
-            for i in range(24):
-    #print(item["SEK_per_kWh"])
-                if json_price[i]["SEK_per_kWh"] < 0.6 :
-                    tomorrows_price.append(i)
-                print(tomorrows_price)
+            best_hours_tomorrow = []
+            json_price.sort(key=takePrice)
+            for i in range(hours_needed):
+                    time = json_price[i]["time_start"]
+                    hours = time.split("T")[1]
+                    time = int(hours.split(":")[0])
+                    best_hours_tomorrow.append(time)
+            print(best_hours_tomorrow)
